cfc9bd5c62d64a5631f4c4d9a64aad6e10b60475
CamiloCastellanos <camilo.castellanos.puentes@gmail.com>
2021-05-18 19:49:26 -0500

    Creacion de clase de prueba para generar libreria

---
+++ holaMundoK.py
@@ -0,0 +1,12 @@
+class Hola:
+    ''' 
+        Saludo personalizado
+    '''
+    def saludo(saludar="K"):
+        print(f"hola mundo {saludar}")
+
+    '''
+        Saludo Generico
+    '''
+    def saludo():
+        print(f"hola mundo a la libreria")
